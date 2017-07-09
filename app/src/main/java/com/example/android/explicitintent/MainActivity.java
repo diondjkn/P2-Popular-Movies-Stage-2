8172c03ee0e42e769115b99c9391e330e870cf54
dion ganteng <dion.djkn@gmail.com>
2017-07-09 23:59:45 +0700

    170709 Gambar

--- app/src/main/java/com/example/android/explicitintent/MainActivity.java
+++ app/src/main/java/com/example/android/explicitintent/MainActivity.java
@@ -23,6 +23,10 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.GridView;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -36,6 +40,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        GridView gridview = (GridView) findViewById(R.id.gridview);
+        gridview.setAdapter(new ImageAdapter(this));
+
+        gridview.setOnItemClickListener(new OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                Toast.makeText(MainActivity.this, "" + position,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
 
         /*
          * Using findViewById, we get a reference to our Button from xml. This allows us to
